704d8225ff3028e927337f1fdcf914774518abd7
s263708 <s263708@uos.ac.uk>
2025-02-27 14:02:42 +0000

    tables

--- BossRanked/models.py
+++ BossRanked/models.py
@@ -2,12 +2,24 @@ from django.db import models
 
 # Create your models here.
 class Game (models.Model):
-    game_ID = models.IntegerField(max_length=5)
+    game_ID = models.IntegerField(max_length=5, primary_key=True)
     game_name = models.CharField(max_length=255)
     genres = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField(max_length=255)
+    description = models.TextField(max_length=1020)
     release_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return f" {self.game_ID}, {self.game_name}, {self.genres}, {self.price}, {self.description}, {self.release_date}"
+
+class User (models.Model):
+    user_ID = models.IntegerField(max_length=5, primary_key=True)
+    game_ID = models.ForeignKey(Game,on_delete=models.CASCADE, related_name="game_id")
+    username = models.CharField(max_length=64)
+    password = models.CharField(max_length=64)
+
+class Reviews (models.Model):
+    review_ID = models.IntegerField(max_length=5, primary_key=True)
+    game_ID = models.ForeignKey(Game,on_delete=models.CASCADE, related_name="game_id")
+    user_ID = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_id")
+    reviews = models.CharField(max_length=1020)
--- BossRanked/views.py
+++ BossRanked/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Game
 
 # Create your views here.
+def home(request):
+    game=Game.objects.all()
+    context={'game':game}
+    return render (request, 'index.html', context )
